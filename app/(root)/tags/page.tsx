bffdfc1aecb098365b81a4977f4201796e86fa1c
Miguel Angel Armenta Acosta <miguel.armenta@salud-digna.org>
2025-05-02 15:52:41 -0700

    Crear actions para filtro por etiqueta

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,8 +1,52 @@
+import TagCard from '@/components/cards/TagCard';
+import DataRenderer from '@/components/DataRenderer';
+import LocalSearch from '@/components/search/LocalSearch';
+import ROUTES from '@/constants/routes';
+import { EMPTY_TAGS } from '@/constants/states';
+import { getTags } from '@/lib/actions/tag.action';
 import React from 'react'
 
-const Tags = () => {
+const Tags = async ({searchParams}:RouteParams) => {
+
+  const {page, pageSize, query, filter} = await searchParams;
+
+  const {success, data, error} = await getTags({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query,
+    filter: filter,
+  });
+
+  const {tags} = data || {};
+  
   return (
-    <div>Tags</div>
+    <>
+      <h1 className='h1-bold text-dark100_light900 text-3xl'>
+        Etiquetas
+      </h1>
+      <section className='mt-11'>
+        <LocalSearch
+          route={ROUTES.TAGS}
+          imgSrc='/icons/search.svg'
+          placeholder='Buscar etiquetas...'
+          iconPosition='left'
+          otherClasses=''
+        />
+      </section>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={tags}
+        empty={EMPTY_TAGS}
+        render={(tags) => (
+          <div className='mt-10 flex w-full flex-wrap gap-4'>
+            {tags.map((tag) => (<TagCard key={tag._id} {...tag}/>))}
+          </div>
+        )}
+      />
+
+    </>
   )
 }
 
--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -10,10 +10,11 @@ interface Props {
     route: string;
     imgSrc: string;
     placeholder: string;
-    otherClasses: string;
+    otherClasses?: string;
+    iconPosition?: 'left' | 'right';
 }
 
-const LocalSearch = ({route, imgSrc, placeholder, otherClasses}: Props) => {
+const LocalSearch = ({route, imgSrc, placeholder, otherClasses, iconPosition="left"}: Props) => {
 
     const pathname = usePathname();
     const router = useRouter();
@@ -52,7 +53,8 @@ const LocalSearch = ({route, imgSrc, placeholder, otherClasses}: Props) => {
     <div className={`background-light800_darkgradient flex min-h-[56px] 
         grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}>
 
-        <Image src={imgSrc} width={24} height={24} alt='search' className='cursor-pointer'/>
+        {iconPosition === "left" && <Image src={imgSrc} width={24} height={24} alt='search' className='cursor-pointer'/>}
+        
         <Input 
             type='text' 
             placeholder={placeholder} 
@@ -61,6 +63,8 @@ const LocalSearch = ({route, imgSrc, placeholder, otherClasses}: Props) => {
             className='paragraph-regular no-focus placeholder text-dark400_light700 border none shadow-none
             outline-none'
         />
+
+    {iconPosition === "right" && <Image src={imgSrc} width={15} height={15} alt='search' className='cursor-pointer'/>}
     </div>
   )
 }
