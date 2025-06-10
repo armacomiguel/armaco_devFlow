4e0c1002ccd63c5c00950fb2951e3040919272ad
Miguel Angel Armenta Acosta <miguel.armenta@salud-digna.org>
2025-06-10 16:21:14 -0700

    modificando logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,24 +1,21 @@
 import pino from 'pino';
 
-
-const isEdge = process.env.NEXT_RUNTIME === "edge";
-const isProduction = process.env.NEXT_RUNTIME === "production";
+const isDev = process.env.NODE_ENV === 'development';
 
 const logger = pino({
-    level: process.env.LOG_LEVEL || "info",
-    transport: !isEdge && !isProduction ? {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            ignore: "pid, hostname",
-            translateTime: "SYS:standard",
-        },
-    }
-    : undefined,
-    formatters: {
-        level: (label) => ({level: label.toUpperCase()}),
+  level: process.env.LOG_LEVEL || "info",
+  transport: isDev ? {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      ignore: "pid,hostname",
+      translateTime: "SYS:standard",
     },
-    timestamp: pino.stdTimeFunctions.isoTime,
+  } : undefined,
+  formatters: {
+    level: (label) => ({ level: label.toUpperCase() }),
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
 });
 
 export default logger;
